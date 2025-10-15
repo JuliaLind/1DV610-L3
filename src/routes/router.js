6214147f9c50a2f83f246aea31abfc304200e145
Julia Lind <lind.julia@hotmail.com>
2025-10-15 19:54:12 +0200

    Update router to serve index.html/webapp at root path

--- src/routes/router.js
+++ src/routes/router.js
@@ -6,19 +6,25 @@
  */
 import createError from 'http-errors'
 import express from 'express'
+import { dirname, join } from 'node:path'
+import { fileURLToPath } from 'node:url'
 
 import { router as apiRouter } from './api/router.js'
 
+
 export const router = express.Router()
+
 
 router.use('/api', apiRouter)
 
-router.get('/',
-  (req, res) => {
-    res.status(200).json({
-      message: 'placeholder, serve index.html here',
-    })
-  })
+const directoryOfFile = dirname(fileURLToPath(import.meta.url))
+
+router.use(express.static(join(directoryOfFile, '../../public')))
+
+router.get('/', (req, res) => {
+  res.sendFile(join(directoryOfFile, '../../public/index.html'))
+})
+
 
 router.use((req, res, next) => {
   next(createError(404))
